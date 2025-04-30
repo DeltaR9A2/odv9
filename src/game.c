65706dea7a81a7db502d5530156bbf0323a612a6
John Colburn <they.called.him.crazy@gmail.com>
2025-04-29 20:25:13 -0500

    Dropping the parser because it overcomplicates things.

--- src/game.c
+++ src/game.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include <assert.h>
 #include <math.h>
 #include <string.h>
 
@@ -17,7 +18,6 @@
 #include "dv_controller.h"
 #include "dv_image_cache.h"
 #include "dv_raster_font.h"
-#include "parser.h"
 
 double clamp(double d, double min, double max) {
   const double t = d < min ? min : d;
@@ -44,16 +44,10 @@ void recalculate_screen_scale_and_position(void){
   ACTIVE_RECT.y = (WIN_CH - ACTIVE_RECT.h)/2;
 }
 
-void toggle_fullscreen(void){
-  if(SDL_GetWindowFlags(WINDOW) & SDL_WINDOW_FULLSCREEN_DESKTOP){
-  }else{
-    SDL_SetWindowFullscreen(WINDOW, SDL_WINDOW_FULLSCREEN_DESKTOP);
-  }
-}
-
 int32_t main_event_watch(void *data, SDL_Event *e){
   if(e->type == SDL_WINDOWEVENT && e->window.event == SDL_WINDOWEVENT_RESIZED){
     WIN_CW = e->window.data1; WIN_CH = e->window.data2;
+    recalculate_screen_scale_and_position();
   }else if(e->type == SDL_QUIT){
     RUNNING = SDL_FALSE;
   }
@@ -104,6 +98,31 @@ int32_t main(void){
   return 0;
 }
 
+
+#define MAX_TITLE_LENGTH 256
+#define MAX_PROSE_LENGTH 1024
+
+#define MAX_OPT_INDEX 5
+#define MAX_OPT_COUNT 6
+#define MAX_OPT_LABEL_LENGTH 128
+
+#define TITLE_WRAP_W 600
+#define PROSE_WRAP_W 360
+#define OPTION_WRAP_W 600
+
+char current_title[MAX_TITLE_LENGTH];
+char current_prose[MAX_TITLE_LENGTH];
+
+typedef void (*scene_t)(void);
+static scene_t next_scene; 
+
+typedef struct{
+  scene_t target;
+  char label[MAX_OPT_LABEL_LENGTH];
+} option_t;
+
+static option_t current_options[MAX_OPT_COUNT];
+
 static font_t *font_header;
 static font_t *font_bright;
 static font_t *font_dimmed;
@@ -113,29 +132,21 @@ static font_t *font_normal;
 
 static SDL_Surface *background_blank;
 static SDL_Surface *background_image;
-static SDL_Surface *inset_image;
-static SDL_Rect bg_dst_rect;    static SDL_Rect bg_src_rect;
-static SDL_Rect inset_dst_rect; static SDL_Rect inset_src_rect;
+static SDL_Rect bg_dst_rect;
+static SDL_Rect bg_src_rect;
 
 static SDL_Surface *pointer_image;
 static SDL_Rect pointer_rect;
 
-static SDL_Rect left_sidebar_rect;
-static SDL_Rect right_sidebar_rect;
-
-static SDL_Surface *slide_buffer;
-static SDL_Rect slide_position;
-static int slide_alpha;
+static SDL_Surface *trans_buffer;
+static SDL_Rect trans_position;
+static int trans_alpha;
 
 static double current_game_step;
 
-#define NUMBER_OF_OPTIONS 6
-#define MAX_OPT_INDEX 5
+static int selected_option_index;
 
-static int option_height;
-static int selected_index;
-
-static scene_t *current_scene = NULL;
+void scn_new_game(void);
 
 void game_state_init(void){
   font_header = font_create("font_alkhemikal_gold.png");
@@ -146,132 +157,154 @@ void game_state_init(void){
   font_invert = font_create("font_mnemonika_16_invert.png");
   font_normal = font_create("font_mnemonika_16_normal.png");
 
-  left_sidebar_rect.w = 0;
-  right_sidebar_rect.w = 0;
-
   background_image = background_blank = get_image("art_blank.png");
-  bg_dst_rect.w = WIN_VW - (left_sidebar_rect.w + right_sidebar_rect.w);
-  bg_dst_rect.h = 360;
-  bg_dst_rect.x = left_sidebar_rect.w;
+  
+  bg_dst_rect.w = WIN_VW;
+  bg_dst_rect.h = WIN_VH;
+  bg_dst_rect.x = 0;
   bg_dst_rect.y = 0;
 
+  bg_src_rect.w = WIN_VW;
   bg_src_rect.h = WIN_VH;
-  bg_src_rect.w = WIN_VW - (left_sidebar_rect.w + right_sidebar_rect.w);
   bg_src_rect.y = (background_image->h - bg_src_rect.h)/2;
   bg_src_rect.x = (background_image->w - bg_src_rect.w)/2;
-
-  inset_image = get_image("crys_cave_west_tunnel_lores.png");
-  inset_dst_rect.w = inset_image->w;
-  inset_dst_rect.h = inset_image->h;
-  inset_dst_rect.x = WIN_VW - (inset_dst_rect.w+16+right_sidebar_rect.w);
-  inset_dst_rect.y = WIN_VH - (inset_dst_rect.h+16);
-  inset_image = NULL; // test
 
   pointer_image = get_image("option_pointer.png");
   pointer_rect.x =  0; pointer_rect.y =  0;
   pointer_rect.w = 16; pointer_rect.h = 16;
 
-  option_height = font_get_height(font_normal)+2;
-
-  slide_buffer = create_surface(WIN_VW, WIN_VH);
-  slide_position.x = 0;
-  slide_position.y = 0;
-  slide_position.w = WIN_VW;
-  slide_position.h = WIN_VH;
-
-  ////////////////
-  parse_file("content.txt");
-  current_scene = get_scene_by_idstr("new_game");
-  ///////////////
+  trans_buffer = create_surface(WIN_VW, WIN_VH);
+  trans_position.x = 0;
+  trans_position.y = 0;
+  trans_position.w = WIN_VW;
+  trans_position.h = WIN_VH;
+  
+  next_scene = scn_new_game;
 }
 
-static FILE *content;
-
 void game_state_step(void){
+  if( next_scene != NULL ){ 
+    SDL_BlitSurface(SCREEN_SURFACE, NULL, trans_buffer, NULL);
+    trans_alpha = 255;
+    next_scene(); 
+  } 
+  
+  // Check for manual game exit.
   if(controller_just_pressed(BTN_BACK)){ RUNNING = SDL_FALSE; }
+
+  // Check for activate fullscreen mode.
   if(controller_just_pressed(BTN_RB)){
     SDL_SetWindowFullscreen(WINDOW, SDL_WINDOW_FULLSCREEN_DESKTOP); }
+  // Check for disable fullscreen mode.
   if(controller_just_pressed(BTN_LB)){
     SDL_SetWindowFullscreen(WINDOW, SDL_FALSE);
     SDL_SetWindowSize(WINDOW, WIN_VW*2, WIN_VH*2); }
-
-  if(controller_just_pressed(BTN_U)){ selected_index -= 1; }
-  if(controller_just_pressed(BTN_D)){ selected_index += 1; }
-
-  if(controller_just_pressed(BTN_START)){
-    if(current_scene->options[selected_index].enabled){
-      current_scene = get_scene_by_idstr(current_scene->options[selected_index].target);
-      selected_index = 0;
-      if(current_scene->bgimg != NULL){
-        background_image = current_scene->bgimg;
-      }else{
-        background_image = background_blank;
-      }
-      slide_position.y = 0;
-      slide_position.x = 0;
-      slide_alpha = 255;
-      SDL_BlitSurface(SCREEN_SURFACE, NULL, slide_buffer, NULL);
-    }
-  }
-
-  if(controller_just_pressed(BTN_RS)){ current_scene = get_scene_by_idstr("new_game"); }
-
-  selected_index = clamp(selected_index, 0, MAX_OPT_INDEX);
-
+  // Check for option selection
+  if(controller_just_pressed(BTN_U)){ selected_option_index -= 1; if(selected_option_index<0){selected_option_index=MAX_OPT_INDEX;}}
+  if(controller_just_pressed(BTN_D)){ selected_option_index += 1; if(selected_option_index>MAX_OPT_INDEX){selected_option_index=0;}}
+  
   SDL_BlitSurface(background_image, &bg_src_rect, SCREEN_SURFACE, &bg_dst_rect);
 
   current_game_step += 1;
 
-  double y = 16 + bg_dst_rect.y;
-  double x = 16 + bg_dst_rect.x;
+  double x=16;
+  double y=16;
 
-  y += font_wrap_string(
-        font_header,
-        current_scene->title,
-        x, y, bg_dst_rect.w - 360,
-        SCREEN_SURFACE);
+  y += font_wrap_string(font_header, current_title, x, y, TITLE_WRAP_W, SCREEN_SURFACE);
 
-  y += font_wrap_string(
-        font_normal,
-        current_scene->prose,
-        x, y, bg_dst_rect.w - 360,
-        SCREEN_SURFACE);
+  y += font_wrap_string(font_normal, current_prose, x, y, PROSE_WRAP_W, SCREEN_SURFACE);
 
-
-  y = WIN_VH - (16 + (option_height * (NUMBER_OF_OPTIONS)));
-  char opt_buffer[256];
-
-  if(inset_image != NULL){
-    SDL_BlitSurface(inset_image, NULL, SCREEN_SURFACE, &inset_dst_rect);
-  }
-
-  x -= pointer_rect.w/2;
+  x -= pointer_rect.w/2; 
   pointer_rect.x = x;
   x = pointer_rect.x + pointer_rect.w + 2;
-  for(int opt_index=0; opt_index <= MAX_OPT_INDEX; opt_index++){
-    option_t *opt = &current_scene->options[opt_index];
-    if(opt->enabled){
-      if(opt_index == selected_index ){
-        font_draw_string(font_golden, opt->label, x, y, SCREEN_SURFACE);
+
+  y += 16;
+
+  for(int i=0; i < MAX_OPT_COUNT; i++){
+    option_t *opt = &current_options[i];
+    
+    if(i == selected_option_index ){
+      pointer_rect.y = y-1;
+      if(opt->target == NULL){
+        y += font_wrap_string(font_dimmed, opt->label, x, y, OPTION_WRAP_W, SCREEN_SURFACE);
       }else{
-        font_draw_string(font_normal, opt->label, x, y, SCREEN_SURFACE);
+        y += font_wrap_string(font_golden, opt->label, x, y, OPTION_WRAP_W, SCREEN_SURFACE);
+        if(controller_just_pressed(BTN_START)){
+          next_scene = current_options[selected_option_index].target;
+          selected_option_index = 0;
+        }
+      }
+    }else{
+      if(opt->target == NULL){
+        y += font_wrap_string(font_dimmed, opt->label, x, y, OPTION_WRAP_W, SCREEN_SURFACE);
+      }else{
+        y += font_wrap_string(font_normal, opt->label, x, y, OPTION_WRAP_W, SCREEN_SURFACE);
       }
     }
-
-    if(opt_index == selected_index ){
-      pointer_rect.y = y-1;
-    }
-
-    y += option_height;
+    y += 2;
   }
 
   SDL_BlitSurface(pointer_image, NULL, SCREEN_SURFACE, &pointer_rect);
 
-  if(slide_position.x < WIN_VW && slide_alpha > 0){
-    SDL_SetSurfaceAlphaMod(slide_buffer, slide_alpha);
-    //SDL_SetSurfaceColorMod(slide_buffer, slide_alpha, slide_alpha, slide_alpha);
-    SDL_BlitSurface(slide_buffer, NULL, SCREEN_SURFACE, &slide_position);
-    //slide_position.x += 0.03*WIN_VW;
-    slide_alpha -= 20;
+  if(trans_alpha > 0){
+    SDL_SetSurfaceAlphaMod(trans_buffer, trans_alpha);
+    SDL_BlitSurface(trans_buffer, NULL, SCREEN_SURFACE, NULL);
+    trans_alpha -= 10;
   }
 }
+
+void reset_scene(void){
+  background_image = background_blank;
+  sprintf(current_title, "%s", "??? ??? ???");
+  sprintf(current_prose, "%s", "... ... ...");
+  for(int i=0; i<MAX_OPT_COUNT; i++){
+    current_options[i].target = NULL;
+    sprintf(current_options[i].label, "%i) %s", i, "...");
+  }
+}
+
+void set_option(int index, scene_t target, const char *label){
+  sprintf(current_options[index].label, "%i) %s", index, label);
+  current_options[index].target = target;
+}
+
+void scn_forest_one(void);
+void scn_forest_two(void);
+void scn_forest_three(void);
+void scn_forest_four(void);
+
+void scn_new_game(void){
+  reset_scene();
+  sprintf(current_title, "%s", "New Game");
+  sprintf(current_prose, "%s", "This is the default new game landing prose.");
+  set_option(0,scn_forest_one,"Go to The Forest 001");
+  next_scene = NULL;
+}
+
+void scn_forest_one(void){
+  reset_scene();
+  background_image = get_image("bg_forest.png");
+  sprintf(current_title, "%s", "The Forest 001");
+  sprintf(current_prose, "%s", "This is the placeholder prose for The Forest 001.");
+  set_option(1,scn_forest_two,"Go to The Forest 002");
+  next_scene = NULL;
+}
+
+void scn_forest_two(void){
+  reset_scene();
+  background_image = get_image("bg_forest_wandering.png");
+  sprintf(current_title, "%s", "The Forest 002");
+  sprintf(current_prose, "%s", "This is the placeholder prose for The Forest 002.");
+  set_option(2,scn_forest_three,"Go to The Forest 003");
+  next_scene = NULL;
+}
+
+void scn_forest_three(void){
+  reset_scene();
+  background_image = get_image("bg_forest_wilderness.png");
+  sprintf(current_title, "%s", "The Forest 003");
+  sprintf(current_prose, "%s", "This is the placeholder prose for The Forest 003.");
+  set_option(5,scn_new_game,"Return to New Game");
+  next_scene = NULL;
+}
+
